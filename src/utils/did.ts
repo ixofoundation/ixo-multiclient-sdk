a0702e133936a7257638c756fed3c9aa214f25fd
Michael Pretorius <michael@ixo.world>
2022-11-22 15:20:58 +0200

    update generateSecpDid util

--- src/utils/did.ts
+++ src/utils/did.ts
@@ -1,7 +1,16 @@
 import base58 from "bs58";
+import { concatArrayBuffers } from "./conversions";
 
+/**
+ * @param pubkey base58 endoded pubkey string
+ */
 export function generateSecpDid(pubkey: string, prefix?: string) {
+  const base58btcCode = "z";
+  const secp256k1PubCode = new Uint8Array([0xe7, 0x01]);
   const pubKeyBz = base58.decode(pubkey);
-  const did = base58.encode(pubKeyBz.slice(0, 16));
+  const encodedpubKey = base58.encode(
+    concatArrayBuffers(secp256k1PubCode, pubKeyBz)
+  );
+  const did = base58btcCode + encodedpubKey;
   return "did:" + (prefix || "ixo") + ":" + did;
 }
