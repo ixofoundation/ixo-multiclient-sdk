d05de22b2edfa0cb11435c718d349bb6e01e61e5
Pieter Pretorius <pieter.pretorius@ixo.world>
2023-04-20 13:34:32 +0200

    fix: fixing findTokenInfoFromDenom if statement and generating ts and docs

--- src/custom_queries/currency.ts
+++ src/custom_queries/currency.ts
@@ -10,12 +10,16 @@ import {
 } from "./currency.types";
 
 export const findTokenFromDenom = (denom: string): TokenAsset | undefined => {
-  const denomLowerCased = denom.toLowerCase();
+  if (!denom?.length) return;
+  const denomLowerCased = denom?.toLowerCase();
   const token = keplrCurrencies[denomLowerCased];
   if (token) return token;
   const possibleToken = Object.values(keplrCurrencies).find(
     (currency) =>
-      currency.coinMinimalDenom === denom || currency.coinDenom === denom
+      currency.coinMinimalDenom === denom ||
+      currency.coinDenom === denom ||
+      currency.coinMinimalDenom === denomLowerCased ||
+      currency.coinDenom === denomLowerCased
   );
   return possibleToken;
 };
@@ -130,11 +134,11 @@ export const findTokenInfoFromDenom = (() => {
         cache[token?.coinGeckoId] ??
         cache[token?.coinMinimalDenom]
       );
-    let coincodexTokenInfo = await fetchTokenInfo(token.coinGeckoId);
+    let coincodexTokenInfo = await fetchTokenInfo(token?.coinGeckoId);
     if (!coincodexTokenInfo)
-      coincodexTokenInfo = await fetchTokenInfo(token.coinDenom);
+      coincodexTokenInfo = await fetchTokenInfo(token?.coinDenom);
     if (!coincodexTokenInfo)
-      coincodexTokenInfo = await fetchTokenInfo(token.coinMinimalDenom);
+      coincodexTokenInfo = await fetchTokenInfo(token?.coinMinimalDenom);
     if (!coincodexTokenInfo) return;
     const result = {
       symbol: coincodexTokenInfo.symbol,
