cb6bf61ed7cd028823e531d43324fe1fb3c420c3
Michael Pretorius <michael@ixo.world>
2024-02-11 16:37:19 +0200

    fix: fix query client exporter with once off tendermint client

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,42 +1,143 @@
-export const createQueryClient = async (rpcEndpoint: string) => {
-  const queryCosmos = await (
-    await import("../codegen")
-  ).cosmos.ClientFactory.createRPCQueryClient({
-    rpcEndpoint,
-  });
-  const queryIxo = await (
-    await import("../codegen")
-  ).ixo.ClientFactory.createRPCQueryClient({
-    rpcEndpoint,
-  });
-  const queryIbc = await (
-    await import("../codegen")
-  ).ibc.ClientFactory.createRPCQueryClient({
-    rpcEndpoint,
-  });
-  const queryCosmWasm = await (
-    await import("../codegen")
-  ).cosmwasm.ClientFactory.createRPCQueryClient({
-    rpcEndpoint,
-  });
-  const queryIca = await (
-    await import("../codegen")
-  ).ica.ClientFactory.createRPCQueryClient({
-    rpcEndpoint,
-  });
-  const queryEntity = await (
-    await import("./entity")
-  ).createRpcQueryExtension(rpcEndpoint);
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+import { ixo, ica, ibc, cosmwasm, cosmos } from "../codegen";
+import {
+  QueryClient as QueryClientStargate,
+  createProtobufRpcClient,
+} from "@cosmjs/stargate";
 
+/**
+ * Creates a new QueryClient with all the modules and custom queries
+ */
+export const createQueryClient = async (rpcEndpoint: string) => {
+  const tmClient = await Tendermint34Client.connect(rpcEndpoint);
+  const client = new QueryClientStargate(tmClient);
+  const rpc = createProtobufRpcClient(client);
+
+  // Manually create the QueryClient with all the modules and custom queries
   return {
-    // add queryIxo and queryIbc first as it has base cosmos module and cosmos namespace needs to be overridden by queryCosmos
-    ...queryIxo,
-    ...queryIca,
-    ...queryIbc,
-    ...queryCosmWasm,
-    ...queryCosmos,
-    custom: {
-      ...queryEntity,
+    // Ixo custom modules
+    // ================================================
+    ixo: {
+      bonds: {
+        v1beta1: new ixo.bonds.v1beta1.QueryClientImpl(rpc),
+      },
+      claims: {
+        v1beta1: new ixo.claims.v1beta1.QueryClientImpl(rpc),
+      },
+      entity: {
+        v1beta1: new ixo.entity.v1beta1.QueryClientImpl(rpc),
+      },
+      iid: {
+        v1beta1: new ixo.iid.v1beta1.QueryClientImpl(rpc),
+      },
+      token: {
+        v1beta1: new ixo.token.v1beta1.QueryClientImpl(rpc),
+      },
+    },
+    // ICA modules
+    // ================================================
+    ica: {
+      intertx: new ica.intertx.QueryClientImpl(rpc),
+    },
+    // IBC modules
+    // ================================================
+    ibc: {
+      applications: {
+        fee: {
+          v1: new ibc.applications.fee.v1.QueryClientImpl(rpc),
+        },
+        interchain_accounts: {
+          controller: {
+            v1: new ibc.applications.interchain_accounts.controller.v1.QueryClientImpl(
+              rpc
+            ),
+          },
+          host: {
+            v1: new ibc.applications.interchain_accounts.host.v1.QueryClientImpl(
+              rpc
+            ),
+          },
+        },
+        transfer: {
+          v1: new ibc.applications.transfer.v1.QueryClientImpl(rpc),
+        },
+      },
+      core: {
+        channel: {
+          v1: new ibc.core.channel.v1.QueryClientImpl(rpc),
+        },
+        client: {
+          v1: new ibc.core.client.v1.QueryClientImpl(rpc),
+        },
+        connection: {
+          v1: new ibc.core.connection.v1.QueryClientImpl(rpc),
+        },
+      },
+    },
+    // IBC modules
+    // ================================================
+    cosmwasm: {
+      wasm: {
+        v1: new cosmwasm.wasm.v1.QueryClientImpl(rpc),
+      },
+    },
+    // Cosmos modules
+    // ================================================
+    cosmos: {
+      app: {
+        v1alpha1: new cosmos.app.v1alpha1.QueryClientImpl(rpc),
+      },
+      auth: {
+        v1beta1: new cosmos.auth.v1beta1.QueryClientImpl(rpc),
+      },
+      authz: {
+        v1beta1: new cosmos.authz.v1beta1.QueryClientImpl(rpc),
+      },
+      bank: {
+        v1beta1: new cosmos.bank.v1beta1.QueryClientImpl(rpc),
+      },
+      base: {
+        tendermint: {
+          v1beta1: new cosmos.base.tendermint.v1beta1.ServiceClientImpl(rpc),
+        },
+      },
+      distribution: {
+        v1beta1: new cosmos.distribution.v1beta1.QueryClientImpl(rpc),
+      },
+      evidence: {
+        v1beta1: new cosmos.evidence.v1beta1.QueryClientImpl(rpc),
+      },
+      feegrant: {
+        v1beta1: new cosmos.feegrant.v1beta1.QueryClientImpl(rpc),
+      },
+      gov: {
+        v1: new cosmos.gov.v1.QueryClientImpl(rpc),
+        v1beta1: new cosmos.gov.v1beta1.QueryClientImpl(rpc),
+      },
+      group: {
+        v1: new cosmos.group.v1.QueryClientImpl(rpc),
+      },
+      mint: {
+        v1beta1: new cosmos.mint.v1beta1.QueryClientImpl(rpc),
+      },
+      nft: {
+        v1beta1: new cosmos.nft.v1beta1.QueryClientImpl(rpc),
+      },
+      params: {
+        v1beta1: new cosmos.params.v1beta1.QueryClientImpl(rpc),
+      },
+      slashing: {
+        v1beta1: new cosmos.slashing.v1beta1.QueryClientImpl(rpc),
+      },
+      staking: {
+        v1beta1: new cosmos.staking.v1beta1.QueryClientImpl(rpc),
+      },
+      tx: {
+        v1beta1: new cosmos.tx.v1beta1.ServiceClientImpl(rpc),
+      },
+      upgrade: {
+        v1beta1: new cosmos.upgrade.v1beta1.QueryClientImpl(rpc),
+      },
     },
   };
 };
---
+++ types/utils/constants.d.ts
@@ -0,0 +1,5 @@
+export declare const MULTIBASE_BASE58BTC_HEADER = "z";
+export declare const MULTICODEC_ED25519_PUB_HEADER: Uint8Array;
+export declare const MULTICODEC_ED25519_PRIV_HEADER: Uint8Array;
+export declare const MULTICODEC_SECP256K1_PUB_HEADER: Uint8Array;
+export declare const MULTICODEC_SECP256K1_PRIV_HEADER: Uint8Array;
