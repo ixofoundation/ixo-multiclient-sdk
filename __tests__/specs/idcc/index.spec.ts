e58481a03fb08f4a085f6055d1762858837cddf1
Alwyn van Wyk <alwyn.vanwyk@ixo.world>
2024-11-28 23:00:21 +0200

    after merge with develop

--- __tests__/specs/idcc/index.spec.ts
+++ __tests__/specs/idcc/index.spec.ts
@@ -26,11 +26,13 @@ beforeAll(() => Promise.all([createQueryClient(), generateWallets(true)]));
 
 // next verify all entities created
 relayerVerifyAllEntities(
-  process.env.ROOT_IMPACTS,
-  "did:ixo:entity:5884e9e2c624cb8a38bde74225215ee8",
+  process.env.ROOT_IMPACTS_TEST,
+  // "did:ixo:entity:5884e9e2c624cb8a38bde74225215ee8", // mainnet relayer
+  "did:ixo:entity:3d079ebc0b332aad3305bb4a51c72edb", // testnet relayer
   chainNetwork,
   [
-    // "did:ixo:entity:ae0a76d510fd7cae66fa17aeaa6df387",
+    "did:ixo:entity:2b8900a43c95c3abf70ccdee49d40efc",
+    "did:ixo:entity:ebee6269e6271037edae4f5a63ad3075",
   ],
   true
 );
