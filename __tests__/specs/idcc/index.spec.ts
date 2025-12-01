daf2316036914c03236516ee696d27e686ec68c0
Alwyn van Wyk <alwyn.vanwyk@ixo.world>
2025-12-01 16:05:29 +0200

    Activate pathgen entities

--- __tests__/specs/idcc/index.spec.ts
+++ __tests__/specs/idcc/index.spec.ts
@@ -26,13 +26,14 @@ beforeAll(() => Promise.all([createQueryClient(), generateWallets(true)]));
 
 // next verify all entities created
 relayerVerifyAllEntities(
-  process.env.ROOT_IMPACTS,
-  "did:ixo:entity:2f22535f8b179a51d77a0e302e68d35d", // mainnet impacts dao relayer
+  process.env.ROOT_QI_SPACE,
+  // "did:ixo:entity:2f22535f8b179a51d77a0e302e68d35d", // mainnet impacts dao relayer
   // "did:ixo:entity:72a27013b1d2f9c3561145e4a424778a", // mainnet emerging dao relayer
   // "did:ixo:entity:3d079ebc0b332aad3305bb4a51c72edb", // testnet relayer
+  "did:ixo:entity:47d9c64891e77cb8e588d33b83ab3042", // mainnet qi space dao relayer
   chainNetwork,
   [
-    "did:ixo:entity:630dd1fc086c5b5ee5eb07f1121ec50f",
+    "did:ixo:entity:2c07c5ce08f9cf9e4eff55d328b3249c",
     // "did:ixo:entity:",
   ],
   true
